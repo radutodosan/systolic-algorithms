e47463d1aef25ea0189a3c9f6eba1d5ecab5bd32
radutodosan02 <radu.todosan02@e-uvt.ro>
2024-12-03 01:31:05 +0200

    added the steps algorithm

--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@ def initialisation():
 
     return matrix, array, rows, cols
 
+
 def print_matrix_array(matrix, array):
     print("A: ")
     for row in matrix:
@@ -21,14 +22,61 @@ def print_matrix_array(matrix, array):
 
     print(array)
 
+
 def add_delays(matrix):
     for i in range(1, len(matrix)):  # start from the second row
         matrix[i] = [None] * i + matrix[i]
     return matrix
 
+
+def add_delays_to_back(matrix):
+    for index, row in enumerate(matrix):
+        for _ in range(len(matrix) - index - 1):
+            row.insert(0, None)
+    return matrix
+
+
+def initialize_processes(matrix_length):
+    return [{
+        'v': 0,
+        'a_in': None,
+        'a_out': None,
+        'u_in': None,
+        'u_out': None
+    } for _ in range(matrix_length)]
+
+
+def step(index, prces, matrix):
+
+    # propagate the input
+    for i in range(len(prces)):
+        prces[i]['a_in'] = matrix[i][index] if index < len(matrix[i]) else 0
+        prces[i]['u_in'] = prces[i + 1]['u_out'] if i < len(prces) - 1 else (u[index] if index < len(u) else 0)
+
+    # execute the algorithm and update the register (v) and the outputs
+    for i in range(len(prces)):
+        prces[i]['v'] += prces[i]['a_in'] * prces[i]['u_in']    if prces[i]['a_in'] and prces[i]['u_in'] else 0
+        prces[i]['a_out'] = prces[i]['a_in']    if prces[i]['a_in'] else 0
+        prces[i]['u_out'] = prces[i]['u_in']    if prces[i]['u_in'] else 0
+
+    index += 1
+
+    return index, prces
+
+
 if __name__ == '__main__':
     A, u, rows, cols = initialisation()
 
-    A = add_delays(A)
+    A = add_delays_to_back(A)
 
     print_matrix_array(A, u)
+
+    input_index = 0
+    processes = initialize_processes(len(A))
+
+    steps = len(A[len(A) - 1]) * (len(A) - 1)
+    progression = 1
+    while progression < steps:
+        input_index, processes = step(input_index, processes, A)
+        print("\nStep", progression, ":", processes)
+        progression += 1
